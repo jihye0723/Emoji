0e759d9a42000e4d1305fd3eda5ad6c0165fcd01
Jiwon <pjw4037@naver.com>
2022-10-27 16:25:17 +0900

    Init netty server (doesn't work)

---
+++ chat-tcp/src/test/java/com/o2a4/chattcp/ChatTcpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.o2a4.chattcp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChatTcpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

0f9f18e4ba3a4f3fbb83149edef6f5d659060739
SSAFY <dojinuk@kakao.com>
2022-11-04 17:02:09 +0900

    Feature kakao login

--- frontend/lib/kakao_login_button.dart
+++ frontend/lib/kakao_login_button.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:practice_01/login/kakao_login.dart';
 import 'package:practice_01/login/main_view_model.dart';
 
+// 카카오톡으로 로그인하는 버튼
 class KakaoLoginButton extends StatefulWidget {
   const KakaoLoginButton({Key? key}) : super(key: key);
 
@@ -18,12 +19,20 @@ class _KakaoLoginButtonState extends State<KakaoLoginButton> {
       children: [
         Text('${viewModel.isLogined}'),
         ElevatedButton(
-            onPressed: () async {
-              await viewModel.login();
-              setState(() {
-              });
-            },
-            child: const Text('Login')),
+          onPressed: () async {
+            await viewModel.login();
+            setState(() {
+            });
+          },
+          style: ElevatedButton.styleFrom(
+            backgroundColor: Colors.transparent,
+            padding: EdgeInsets.symmetric(horizontal: 0, vertical: 0),
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(12)
+            ),
+          ),
+          child: Image.asset('assets/images/icon-kakao-login.png'),
+        ),
       ],
     );
   }

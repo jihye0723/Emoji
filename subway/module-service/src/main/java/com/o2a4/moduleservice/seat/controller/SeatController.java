7f34979966f604db4c6da49665306ef12273c7f8
jihye0723 <88600739+jihye0723@users.noreply.github.com>
2022-11-05 18:24:03 +0900

    feat: redis-save

--- subway/module-service/src/main/java/com/o2a4/moduleservice/seat/controller/SeatController.java
+++ subway/module-service/src/main/java/com/o2a4/moduleservice/seat/controller/SeatController.java
@@ -8,9 +8,11 @@ import org.springframework.data.redis.core.ReactiveRedisTemplate;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
 import java.util.Map;
 import java.util.Timer;
 import java.util.TimerTask;
+import java.util.concurrent.TimeUnit;
 
 @RestController
 @RequestMapping("/seat")
@@ -18,8 +20,7 @@ import java.util.TimerTask;
 @Slf4j
 public class SeatController {
 
-   private final ReactiveRedisTemplate<String, String> reactiveRedisTemplate;
-
+    private final RedisTemplate<String, String> redisTemplate;
 
     // 채팅서버로부터 양도자 id 받아서, redis 생성 > 5초 후 1명 뽑아서 양도자id + 당첨자id 보내주기
     // WebClient 로 통신하면 될듯 ?
@@ -28,6 +29,11 @@ public class SeatController {
         log.info(userid);
         // userid : 자리 양도한 사람 아이디
         String key = "seat:"+userid; // ex) key = seat:ssafy
+
+        // redis 생성
+        redisTemplate.opsForList().rightPush(key, "start");
+        redisTemplate.expire(key, 180, TimeUnit.SECONDS); // 3분동안 redis 에 저장
+
         Timer t= new Timer();
 
         //redis 에서 한명 뽑기
@@ -47,16 +53,12 @@ public class SeatController {
         String attend_id = seatMap.get("attend_id");
         // 자리양도 시작한 사용자 아이디
         String start_id = seatMap.get("start_id");
+        //log.info("참가자 : " + attend_id + " , 양도자 : "+ start_id);
 
-        System.out.println("참가자 : " + attend_id + " , 양도자 : "+ start_id);
         // userid : 자리 양도한 사람 아이디
         String key = "seat:"+start_id; // ex) key = seat:ssafy
-        log.debug("redis key : "+  key);
-        reactiveRedisTemplate.opsForValue().set(key, attend_id);
 
-
-
-
+        redisTemplate.opsForList().rightPush(key, attend_id);
 
     }
     class Winning extends TimerTask{
@@ -67,7 +69,17 @@ public class SeatController {
         }
         @Override
         public void run() {
-            // redis 에서 한명뽑기
+            // redis 에서 한명뽑기 ( 1부터 뽑아야함 )
+            // size
+            Long size= redisTemplate.opsForList().size(key);
+            List<String> list= redisTemplate.opsForList().range(key, 1, size-1);
+            // 참가자 목록 얻어오기
+            for(int i=0; i<list.size(); i++){
+                System.out.println(list.get(i));
+            }
+
+
+
         }
     }
 }

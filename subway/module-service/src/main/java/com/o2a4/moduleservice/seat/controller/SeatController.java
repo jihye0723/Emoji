766ecedffe24e4bf0708eb2bfcb9c43eaf1d2b0b
jihye0723 <88600739+jihye0723@users.noreply.github.com>
2022-11-07 00:17:36 +0900

    feat: redis-save

--- subway/module-service/src/main/java/com/o2a4/moduleservice/seat/controller/SeatController.java
+++ subway/module-service/src/main/java/com/o2a4/moduleservice/seat/controller/SeatController.java
@@ -3,15 +3,12 @@ package com.o2a4.moduleservice.seat.controller;
 
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.redis.core.ReactiveRedisTemplate;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.List;
-import java.util.Map;
-import java.util.Timer;
-import java.util.TimerTask;
+import javax.xml.ws.Response;
+import java.util.*;
 import java.util.concurrent.TimeUnit;
 
 @RestController
@@ -25,25 +22,41 @@ public class SeatController {
     // 채팅서버로부터 양도자 id 받아서, redis 생성 > 5초 후 1명 뽑아서 양도자id + 당첨자id 보내주기
     // WebClient 로 통신하면 될듯 ?
     @GetMapping("/{userid}")
-    public void seatCompeteOpen(@PathVariable String userid){
+    public Map<String,String> seatCompeteOpen(@PathVariable String userid) throws InterruptedException {
         log.info(userid);
         // userid : 자리 양도한 사람 아이디
         String key = "seat:"+userid; // ex) key = seat:ssafy
-
         // redis 생성
         redisTemplate.opsForList().rightPush(key, "start");
         redisTemplate.expire(key, 180, TimeUnit.SECONDS); // 3분동안 redis 에 저장
+//        /*Timer 사용했을 떄 */
+//        Timer t= new Timer();
+//        //redis 에서 한명 뽑기
+//        Winning wt = new Winning(key);
+//        //5(+1)초가 지난후에, 해당 작업 실행
+//        t.schedule(wt, 6000);
 
-        Timer t= new Timer();
+        /*Thread.sleep 사용했을 때 */
+        try{
+            Thread.sleep(6000);
+        }catch(InterruptedException e){
+            e.printStackTrace();
+        }
+        Long size= redisTemplate.opsForList().size(key);
+        // 참가자 목록 얻어오기 : list
+        List<String> list= redisTemplate.opsForList().range(key, 1, size-1);
 
-        //redis 에서 한명 뽑기
-        Winning wt = new Winning(key);
-        //5초가 지난후에, 해당 작업 실행
-        t.schedule(wt, 5000);
+        Random random = new Random();
+        int randomIndex = random.nextInt(list.size());
+        String win_id = list.get(randomIndex);
 
+        Map<String, String> map = new HashMap<>();
+        map.put("UserId", userid);
+        map.put("WinnerId", win_id);
+        log.info("당첨자 id : " + win_id +", 양도자 id : "+ userid);
 
+        return map;
     }
-
 
     // 자리양도 주최한 사람 + 자리양도 신청한 신청자
     // 신청자 id redis 에 넣기
@@ -59,27 +72,26 @@ public class SeatController {
         String key = "seat:"+start_id; // ex) key = seat:ssafy
 
         redisTemplate.opsForList().rightPush(key, attend_id);
-
     }
-    class Winning extends TimerTask{
-        private String key;
+//    class Winning extends TimerTask{
+//        private String key;
+//
+//        public Winning(String key){
+//            this.key=key;
+//        }
+//        @Override
+//        public void run() {
+//            // redis 에서 한명뽑기 ( 1부터 뽑아야함 )
+//            // size
+//            Long size= redisTemplate.opsForList().size(key);
+//            // 참가자 목록 얻어오기 : list
+//            List<String> list= redisTemplate.opsForList().range(key, 1, size-1);
+//
+//            Random random = new Random();
+//            int randomIndex = random.nextInt(list.size());
+//            String win_id = list.get(randomIndex);
+//            log.info("당점자 : " +win_id);
+//        }
+//    } // winning class
 
-        public Winning(String key){
-            this.key=key;
-        }
-        @Override
-        public void run() {
-            // redis 에서 한명뽑기 ( 1부터 뽑아야함 )
-            // size
-            Long size= redisTemplate.opsForList().size(key);
-            List<String> list= redisTemplate.opsForList().range(key, 1, size-1);
-            // 참가자 목록 얻어오기
-            for(int i=0; i<list.size(); i++){
-                System.out.println(list.get(i));
-            }
-
-
-
-        }
-    }
 }

8b50841ccce547dad005866530d1fe05f20352d1
최지혜 <chlwlgp11@naver.com>
2022-11-11 16:54:31 +0900

    feat:seat config

---
+++ subway/module-service/src/main/java/com/o2a4/moduleservice/seat/dto/SeatsRepository.java
@@ -0,0 +1,12 @@
+package com.o2a4.moduleservice.seat.dto;
+
+
+import org.springframework.stereotype.Component;
+
+import java.util.Set;
+
+@Component
+public class SeatsRepository {
+//    private final Set<String>
+
+}

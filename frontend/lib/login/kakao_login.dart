effae5b1e5288b25357063a8e9509e85c3e5b760
Chan Jeong <saorin12@naver.com>
2022-11-14 15:48:31 +0900

    fix 새로고침 수정

--- frontend/lib/login/kakao_login.dart
+++ frontend/lib/login/kakao_login.dart
@@ -1,23 +1,40 @@
 import 'package:kakao_flutter_sdk_user/kakao_flutter_sdk_user.dart';
 import 'package:practice_01/login/social_login.dart';
+import 'package:http/http.dart' as http;
 /*
 * 토큰이 있다면, 로그인 페이지를 띄워주지 않아도 됨.
 * 토큰이 없다면, 로그인 페이지를 띄워주어 로그인 할 수 있도록 함.
 */
 class KakaoLogin implements SocialLogin {
+
+  // url:8081/oauth/kakao-login?access-token=
+  // 여기서 tkn 은 카카오톡에서 받아온 access token 을 뜻함.
+  _postRequest(String tkn) async {
+    // String baseUrl = 'http://localhost:8081/oauth/kakao-login?';
+    // baseUrl += 'access-token=$tkn';
+
+    // 안드로이드 에뮬레이터가 localhost대신 10.0.2.2를 사용하므로,
+    var uri = Uri.http('10.0.2.2:8081', '/oauth/kakao-login', { 'access-token' : tkn });
+
+    var res = await http.post(uri);
+    return res;
+
+  }
+
   @override
-  Future<bool> login() async {
+  Future login() async {
+
     // 카카오톡이 설치되어 있는지를 true, false로 받아온다.
     bool isInstalled = await isKakaoTalkInstalled();
     // 설치되어있는지 확인하고 그에 맞는 로그인 절차 진행
     if (isInstalled) {
       try {
-        OAuthToken token = await UserApi.instance.loginWithKakaoAccount();
-        print('로그인 성공 ${token.accessToken}');
-        return true;
+        OAuthToken token = await UserApi.instance.loginWithKakaoTalk();
+        print('카카오톡으로 로그인(인증) 성공 ${token.accessToken}');
+        return _postRequest(token.accessToken);
       } catch (error) {
-        print('로그인 실패 $error');
-        return false;
+        print('카카오톡으로 로그인(인증) 실패 $error');
+        return null;
       }
     }
     // 카카오톡이 설치되어 있지 않다면,
@@ -25,12 +42,12 @@ class KakaoLogin implements SocialLogin {
       try {
         // 카카오 계정으로 로그인 유도
         OAuthToken token = await UserApi.instance.loginWithKakaoAccount();
-        print('로그인 성공 ${token.accessToken}');
-        return true;
+        print('카카오 계정으로 로그인(인증) 성공 ${token.accessToken}');
+        return _postRequest(token.accessToken);
       }
       catch (error) {
-        print('로그인 실패 $error');
-        return false;
+        print('카카오 계정으로 로그인(인증) 실패 $error');
+        return null;
       }
     }
   }
--- frontend/lib/main.dart
+++ frontend/lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 import 'package:kakao_flutter_sdk_user/kakao_flutter_sdk_user.dart';
 import 'package:flutter_native_splash/flutter_native_splash.dart';
 import 'package:practice_01/chatpage/chat.dart';
@@ -9,9 +10,7 @@ import 'package:video_player/video_player.dart';
 
 // 메인 함수
 void main() async {
-  KakaoSdk.init(
-    nativeAppKey: '9e8f2d7dc89324523cecae5569e5f764',
-  );
+  KakaoSdk.init(nativeAppKey: '20a57c799c063a61eaa19958e34c58cb',);
   await ScreenUtil.ensureScreenSize();
   runApp(const MyApp());
 }
@@ -23,22 +22,28 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return ScreenUtilInit(
-        designSize: const Size(360, 800),
-        minTextAdapt: true,
-        splitScreenMode: true,
-        builder: (context, child) {
-          return MaterialApp(
-              debugShowCheckedModeBanner: false,
-              title: "메인페이지",
-              theme: ThemeData(
-                primarySwatch: Colors.blue,
-              ),
-              home: SplashScreen());
-        });
+      designSize: const Size(360, 800),
+      minTextAdapt: true,
+      splitScreenMode: true,
+      builder: (context, child) {
+        return MaterialApp(
+          debugShowCheckedModeBanner: false,
+          title: "메인페이지",
+          theme: ThemeData(
+            primarySwatch: Colors.blue,
+          ),
+          home: SplashScreen()
+        );
+      }
+    );
   }
 }
 
 // 스플래시 스크린을 띄우는 동안, 토큰이 있는지 확인하기
+/*
+* To do.
+*   Access 토큰 만료되었으면, Refresh 토큰이 유효한지 확인하고 재발급 받는 로직 필요.
+* */
 class SplashScreen extends StatefulWidget {
   const SplashScreen({Key? key}) : super(key: key);
 
@@ -48,22 +53,14 @@ class SplashScreen extends StatefulWidget {
 
 class _SplashScreenState extends State<SplashScreen> {
   late VideoPlayerController _controller;
-  bool weHaveToken = false;
+  String? userAccessToken;
+  static final storage = FlutterSecureStorage();
 
-  void hasToken() async {
-    if (await AuthApi.instance.hasToken()) {
-      try {
-        AccessTokenInfo tokenInfo = await UserApi.instance.accessTokenInfo();
-        weHaveToken = true;
-        print('토큰 유효성 체크 성공 ${tokenInfo.id} ${tokenInfo.expiresIn}');
-      } catch (error) {
-        if (error is KakaoException && error.isInvalidTokenError()) {
-          print('토큰 만료 $error');
-        } else {
-          print('토큰 정보 조회 실패 $error');
-        }
-      }
-    }
+  // 저장되어 있는 유저의 accessToken 을 확인한다.
+  getUserToken() async {
+    //read 를 통해 accessToken 을 불러온다. 데이터가 없을 때는 null 반환
+    userAccessToken = await storage.read(key: 'accessToken');
+    return userAccessToken;
   }
 
   @override
@@ -76,48 +73,56 @@ class _SplashScreenState extends State<SplashScreen> {
       ..initialize().then((_) {})
       ..setVolume(0.0);
 
+    //비동기로 getUserToken 함수를 실행하여 Secure Storage 정보를 불러오는 작업.
+    WidgetsBinding.instance.addPostFrameCallback((_) {
+      getUserToken();
+    });
+
     _playVideo();
+  }
+
+  void _playVideo() async {
+    //playing video
+    _controller.play();
+    //add delay till video is complete
+    await Future.delayed(const Duration(seconds: 4));
+
+    // 이미 저장되어 있는 AccessToken 이 존재한다면,
+    if (userAccessToken != null) {
+      print('저장되어 있는 토큰 발견!!');
+      // 홈 화면으로 이동하게 된다.
+      Navigator.of(context).pop();
+      Navigator.push(context, MaterialPageRoute(builder: (context) => Home()));
+    }
+    // 저장되어 있는 AccessToken 이 존재하지 않는다면,
+    else {
+      print('저장되어 있는 토큰이 없습니다!!');
+      // 로그인 페이지로 이동하게 된다.
+      Navigator.of(context).pop();
+      Navigator.push(context, MaterialPageRoute(builder: (context) => LoginPage()));
+    }
   }
 
   @override
   void dispose() {
     _controller.dispose();
     super.dispose();
-
-    if (weHaveToken) {
-      /*
-      * 토큰이 있다면, 홈 화면으로 넘어가면서 이메일을 같이 보내줘야 함.
-      */
-      // navigating to home screen
-      Navigator.of(context).pop();
-      Navigator.push(context, MaterialPageRoute(builder: (context) => Home()));
-      // Navigator.push(context, MaterialPageRoute(builder: (context) => LoginPage()));
-    } else {
-      //토큰이 없다면, 로그인 페이지로 넘겨주어야 한다.
-      // Navigator.push(context, MaterialPageRoute(builder: (context) => LoginPage()));
-      Navigator.push(context, MaterialPageRoute(builder: (context) => Home()));
-    }
-  }
-
-  void _playVideo() async {
-    //playing video
-    _controller.play();
-    hasToken();
-    //add delay till video is complete
-    await Future.delayed(const Duration(seconds: 4));
-    dispose();
   }
 
   @override
   Widget build(BuildContext context) {
     ScreenUtil.init(context, designSize: const Size(360, 800));
     return Scaffold(
-        backgroundColor: Colors.white,
-        body: Center(
-            child: AspectRatio(
-                aspectRatio: _controller.value.aspectRatio,
-                child: VideoPlayer(
-                  _controller,
-                ))));
+      backgroundColor: Colors.white,
+      body: Center(
+        child:AspectRatio(
+          aspectRatio: _controller.value.aspectRatio,
+          child: VideoPlayer(
+            _controller,
+          )
+        )
+      )
+    );
   }
 }
+

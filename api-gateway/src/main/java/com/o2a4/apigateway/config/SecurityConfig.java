322b8fcc359255a7970772dff30e2f14016801a0
hyjin23 <hyjin23>
2022-11-17 17:11:06 +0900

    [feature] security setting

--- api-gateway/src/main/java/com/o2a4/apigateway/config/SecurityConfig.java
+++ api-gateway/src/main/java/com/o2a4/apigateway/config/SecurityConfig.java
@@ -6,18 +6,21 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.server.SecurityWebFilterChain;
 
 @Slf4j
 @Configuration
 @EnableWebFluxSecurity
 @AllArgsConstructor
 public class SecurityConfig {
+
     @Bean
-    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+    public SecurityWebFilterChain filterChain(ServerHttpSecurity http) throws Exception {
         return http
+//.authorizeExchange().anyExchange().authenticated().and()
                .csrf().disable()
                .build();
     }
-
 }

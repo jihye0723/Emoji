33522e495a35b8198a80da471a5f3982ee85085a
hyjin23 <hyjin23>
2022-11-17 16:35:19 +0900

    [feature] security setting

--- api-gateway/src/main/java/com/o2a4/apigateway/config/SecurityConfig.java
+++ api-gateway/src/main/java/com/o2a4/apigateway/config/SecurityConfig.java
@@ -4,12 +4,11 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
-@EnableWebSecurity
-@RequiredArgsConstructor
+@EnableWebFluxSecurity
 public class SecurityConfig {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {

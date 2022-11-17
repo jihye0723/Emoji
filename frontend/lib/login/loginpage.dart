79550e1ad87c7174ceed96f4f8b51c3df95bfb10
dojinuk <dojinuk@nate.com>
2022-11-17 16:12:00 +0900

    Design logo and loginpage

--- frontend/lib/login/loginpage.dart
+++ frontend/lib/login/loginpage.dart
@@ -20,40 +20,56 @@ class _LoginPageState extends State<LoginPage> {
     return Material(
       child: Container(
           decoration: BoxDecoration(
-            color: Color(0xffFFE15D),
-          ),
-          child: Center(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Spacer(flex: 4),
-                Image.asset('assets/images/train_example.png', width: 250),
-                SizedBox(
-                  width: 360,
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    crossAxisAlignment: CrossAxisAlignment.end,
-                    children: [
-                      Spacer(flex: 1),
-                      Text('이', style: TextStyle(fontFamily: customFont, fontSize: 60, color: Color(customColor)),),
-                      Text('름', style: TextStyle(fontFamily: customFont, fontSize: 50, color: Color(customColor)),),
-                      Spacer(flex: 1),
-                      Text('모', style: TextStyle(fontFamily: customFont, fontSize: 60, color: Color(customColor)),),
-                      Text('를', style: TextStyle(fontFamily: customFont, fontSize: 50, color: Color(customColor)),),
-                      Spacer(flex: 1),
-                      Text('지', style: TextStyle(fontFamily: customFont, fontSize: 60, color: Color(customColor)),),
-                      Text('하철', style: TextStyle(fontFamily: customFont, fontSize: 50, color: Color(customColor)),),
-                      Spacer(flex: 1),
-                    ],
-                  ),
-                ),
-                Spacer(flex: 2),
-                KakaoLoginButton(),
-                Spacer(flex: 3),
-              ],
+            image: DecorationImage(
+              fit: BoxFit.cover,
+              image: AssetImage('assets/images/loginpage-background-image.png'),
             ),
+          ),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Spacer(flex: 8),
+              KakaoLoginButton(),
+              Spacer(flex: 2),
+            ],
           )
-      )
+      ),
+      // child: Container(
+      //     decoration: BoxDecoration(
+      //       color: Color(0xffFFE15D),
+      //     ),
+      //     child: Center(
+      //       child: Column(
+      //         mainAxisAlignment: MainAxisAlignment.center,
+      //         children: [
+      //           // Spacer(flex: 4),
+      //           // Image.asset('assets/images/train_example.png', width: 250),
+      //           // SizedBox(
+      //           //   width: 360,
+      //           //   child: Row(
+      //           //     mainAxisAlignment: MainAxisAlignment.center,
+      //           //     crossAxisAlignment: CrossAxisAlignment.end,
+      //           //     children: [
+      //           //       Spacer(flex: 1),
+      //           //       Text('이', style: TextStyle(fontFamily: customFont, fontSize: 60, color: Color(customColor)),),
+      //           //       Text('름', style: TextStyle(fontFamily: customFont, fontSize: 50, color: Color(customColor)),),
+      //           //       Spacer(flex: 1),
+      //           //       Text('모', style: TextStyle(fontFamily: customFont, fontSize: 60, color: Color(customColor)),),
+      //           //       Text('를', style: TextStyle(fontFamily: customFont, fontSize: 50, color: Color(customColor)),),
+      //           //       Spacer(flex: 1),
+      //           //       Text('지', style: TextStyle(fontFamily: customFont, fontSize: 60, color: Color(customColor)),),
+      //           //       Text('하철', style: TextStyle(fontFamily: customFont, fontSize: 50, color: Color(customColor)),),
+      //           //       Spacer(flex: 1),
+      //           //     ],
+      //           //   ),
+      //           // ),
+      //           Spacer(flex: 2),
+      //
+      //           Spacer(flex: 1),
+      //         ],
+      //       ),
+      //     )
+      // )
     );
   }
 }
